8614487bb75b82c50aac27a583289cc8ebb5fbcc
shubhi2108 <shubhi.j08@gmail.com>
2016-11-28 21:08:06 -0500

    Changes in Model

--- random_forest.py
+++ random_forest.py
@@ -16,15 +16,9 @@ def main():
     
     sc.setLogLevel("INFO")
 
-    df = spark.read.parquet(sys.argv[1])
+    train_df = spark.read.parquet(sys.argv[1])
 
-    # df = df.withColumn("label", df.label.cast("double"))
-    df_split = df.randomSplit([0.8, 0.2], 9)
-
-    train_df =  df_split[0]
-    test_df = df_split[1]
-
-    rfc = RandomForestClassifier(maxDepth=8, maxBins=2400000, numTrees=100)
+    rfc = RandomForestClassifier(maxDepth=8, maxBins=2400000, numTrees=128,impurity="gini")
     rfc_model = rfc.fit(train_df)
     rfc_model.save(sys.argv[2] + "rfc_model")
 
