644b2a2fc173700a255a5e4917591239ee128637
pranjaypatil <pranjaypatil@hotmail.com>
2016-11-30 01:31:36 -0500

    now calculates accuracy, precision, recall

--- rf_transform_model.py
+++ rf_transform_model.py
@@ -19,15 +19,31 @@ def main():
     # Loading the test data
     df_test= spark.read.parquet(sys.argv[1])
 
+    df_test, df_discard = df_test.randomSplit([0.2, 0.8])
+
     # Load the model
     rf_model=RandomForestClassificationModel.load(sys.argv[2])
     
     # Make the predictions
     predictions = rf_model.transform(df_test)
     
-    predictionsRDD=predictions.rdd
+    #predictionsRDD=predictions.rdd
 
-    predictionsRDD.saveAsTextFile(sys.argv[3]+"output.text")
+    #predictionsRDD.saveAsTextFile(sys.argv[3]+"output.text")
+
+    evaluator_acc = MulticlassClassificationEvaluator(predictionCol="prediction", labelCol="label", metricName="accuracy")
+    accuracy = evaluator_acc.evaluate(predictions)
+
+    print "accuracy *******************"
+    print accuracy
+
+    evaluator_pre = MulticlassClassificationEvaluator(predictionCol="prediction", labelCol="label", metricName="weightedPrecision")
+    
+    print "precision *******************"
+    print evaluator_pre.evaluate(predictions)
+
+    print "recall **********************"
+    print MulticlassClassificationEvaluator(predictionCol="prediction", labelCol="label", metricName="weightedRecall").evaluate(predictions)
 
 
 if __name__ == '__main__':
